812976abcb96e3244a8032a08ee72929c1588c7d
Javad Gurbanov <gurbanovcavad7@gmail.com>
2026-02-21 10:32:47 +0400

    Update Config Model

--- src/asr_scraper/config.py
+++ src/asr_scraper/config.py
@@ -1,9 +1,20 @@
-from typing import Optional
+from typing import Optional, List
 from datetime import date
 from pathlib import Path
 
 class Config:
-    def __init__(self, channels: str, output_dir: Path = Path("./data"), sample_rate: int = 16000, mono: bool = True, pcm_bit_depth: int = 16, concurrency: int = 2, max_videos_per_channel: Optional[int] = None, since_date: Optional[date] = None, skip_existing: bool = True, write_manifest: Path = Path("./manifest.jsonl")):
+    def __init__(self, 
+                channels: List[str] = [], 
+                output_dir: Path = Path("./data"), 
+                sample_rate: int = 16000, 
+                mono: bool = True, 
+                pcm_bit_depth: int = 16, 
+                concurrency: int = 2, 
+                max_videos_per_channel: Optional[int] = None, 
+                since_date: Optional[str] = None, 
+                skip_existing: bool = True, 
+                write_manifest: Path = Path("./manifest.jsonl"),
+        ):
         self.channels = channels
         self.output_dir = output_dir
         self.sample_rate = sample_rate
@@ -15,3 +26,5 @@ class Config:
         self.skip_existing = skip_existing
         self.write_manifest = write_manifest
         # platform_overrides: 
+        
+config = Config()
