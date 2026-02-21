03bbf5326d734af43a6188435287cbf93c5f492b
Javad Gurbanov <gurbanovcavad7@gmail.com>
2026-02-21 14:16:03 +0400

    Update Slugify Function

--- src/asr_scraper/utils/slugify.py
+++ src/asr_scraper/utils/slugify.py
@@ -1,30 +1,33 @@
 import re 
 from unicodedata import normalize
+from pathlib import Path
 
-def slugify(a: str, st: set, video_id: str) -> str:
+def slugify(a: str, output_dir: Path, video_id: str) -> Path:
     slug = normalize("NFKC", a).lower()
     slug= re.sub(r"[^\w\s-]", "", slug)
     slug = re.sub(r"[-\s]+", "-", slug).strip("-_")
 
-    return unique_slug(slug, st, video_id)
+    return unique_slug(slug, output_dir, video_id)
 
     # slug = normalize("NFKD", a).encode("ascii", "ignore").decode("utf-8").lower()
     # slug = re.sub(r'[^a-z0-9]+', '-', slug).strip('-')
     
     # return unique_slug(slug, st, video_id)
     
-def unique_slug(slug: str, slugs: set, video_id: str) -> str:
-    if slug not in slugs:
-        return slug
-   
-    res = f"{slug}-{video_id}"
+def unique_slug(slug: str,output_dir: Path, video_id: str) -> Path:
+    output_path = output_dir / f"{slug}.wav"
     
-    if res not in slugs:
-        return res
+    if not output_path.exists():
+        return output_path
+    
+    output_path = output_dir / f"{slug}-{video_id}.wav"
+    
+    if not output_path.exists():
+        return output_path
     
     # just in case
     c = 1
-    while f"{slug}-{c}" in slugs:
+    while (output_dir / f"{slug}-{c}.wav").exists():
         c += 1
         
-    return f"{slug}-{c}"
+    return output_dir / f"{slug}-{c}.wav"
